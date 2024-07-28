5ff0a81107f3abdcae37111b19bc22d351d5effc
Rick South <rick@natamus.com>
2024-07-28 02:33:54 +0200

    (v3.9) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Forge/src/main/java/com/natamus/zombievillagersfromspawner/ModForge.java
+++ Forge/src/main/java/com/natamus/zombievillagersfromspawner/ModForge.java
@@ -1,6 +1,7 @@
 package com.natamus.zombievillagersfromspawner;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.zombievillagersfromspawner.forge.config.IntegrateForgeConfig;
 import com.natamus.zombievillagersfromspawner.util.Reference;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -13,6 +14,10 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ModForge {
 	
 	public ModForge() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 		modEventBus.addListener(this::loadComplete);
 
