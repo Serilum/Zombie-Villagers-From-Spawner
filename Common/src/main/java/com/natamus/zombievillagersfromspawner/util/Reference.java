58cfed02d528c3bc3e1ee0dda2e8586147cb0271
Rick South <rick@natamus.com>
2024-08-09 07:58:54 +0200

    Zombie Villagers From Spawner source code for 1.21.1.

---
+++ Common/src/main/java/com/natamus/zombievillagersfromspawner/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.zombievillagersfromspawner.util;
+
+public class Reference {
+	public static final String MOD_ID = "zombievillagersfromspawner";
+	public static final String NAME = "Zombie Villagers From Spawner";
+	public static final String VERSION = "3.9";
+	public static final String ACCEPTED_VERSIONS = "[1.21.1]";
+}
